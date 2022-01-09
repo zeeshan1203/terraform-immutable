4cad6f9b607d457529a3c91ed7777ebbad707017
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-09 11:47:41 +0530

    added & updated and modified

--- asg/data.tf
+++ asg/data.tf
@@ -1,7 +1,7 @@
-data "aws_ami" "component-ami" {
+data "aws_ami" "centos7" {
   most_recent      = true
-  name_regex       = "^${var.COMPONENT}-*"
-  owners           = ["self"]
+  name_regex       = "^Centos-7-DevOps-Practice"
+  owners           = ["973714476881"]
 }
 
 data "terraform_remote_state" "vpc" {
